767ac8f12a842accb388a6f129be686dc19c656a
Bruno Van de Velde <vdv_b@tgui.eu>
2014-12-21 12:40:04 +0100

    Silently discard agent sollicitation when needed

--- click-2.0.1/elements/local/checkifagentsolicitation.cc
+++ click-2.0.1/elements/local/checkifagentsolicitation.cc
@@ -27,9 +27,33 @@ void CheckIfAgentSolicitation::push(int, Packet* packet) {
     click_ip* iph = (click_ip*)(ethh + 1);
     if (iph->ip_p == 1) // ICMP packet
     {
-        agent_solicitation_header* advh = (agent_solicitation_header*)(iph + 1);
-        if (advh->type == 10) // Agent Solicitation
+        agent_solicitation_header* ash = (agent_solicitation_header*)(iph + 1);
+        if (ash->type == 10) // Agent Solicitation
         {
+            // Silently discard packet if source address is 0
+            if (iph->ip_src == 0) {
+                packet->kill();
+                return;
+            }
+
+            // Silently discard packet if ICMP Checksum is invalid
+            if (click_in_cksum((unsigned char*)ash, sizeof(agent_solicitation_header)) != 0) {
+                packet->kill();
+                return;
+            }
+
+            // Silently discard packet if ICMP Code is not 0
+            if (ash->code != 0) {
+                packet->kill();
+                return;
+            }
+
+            // Silently discard packet if ICMP length (derived from the IP length) is not 8 or more octets
+            if (ntohs(iph->ip_len) >= 8 + sizeof(click_ip)) {
+                packet->kill();
+                return;
+            }
+
             output(0).push(packet);
             return;
         }
